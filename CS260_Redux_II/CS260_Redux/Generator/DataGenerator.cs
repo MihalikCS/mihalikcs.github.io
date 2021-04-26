c136c2f425228a52e75567fdbcf0e914b417cc5f
Michael Mihalik <45341659+mmihalikcs@users.noreply.github.com>
2021-04-25 21:57:27 -0400

    Add files via upload

---
+++ CS260_Redux_II/CS260_Redux/Generator/DataGenerator.cs
@@ -0,0 +1,87 @@
+﻿using CS260_Redux.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS260_Redux.Generator
+{
+    public class DataGenerator
+    {
+        int _MAX_SIZE;
+        List<FileDTO> _Table;
+        List<string> _NameDict, _AddressDict;
+        Random _Rand;
+
+        public DataGenerator(int size)
+        {
+            _MAX_SIZE = size;
+            _Table = new List<FileDTO>(1000);
+
+            // Dictionaries
+            _NameDict = new List<string>()
+            {
+                "Gary",
+                "Bob",
+                "George",
+                "Sam",
+                "Harper",
+                "Cora"
+            };
+
+            _AddressDict = new List<string>()
+            {
+                "123 Seasme St",
+                "454 Brick Ave",
+                "375 Long Point Rd",
+                "104 Ridge Ave",
+                "1034 Dode Lane"
+            };
+
+            // Seed Rand
+            _Rand = new Random();
+        }
+
+        public void GenerateData()
+        {
+            // Generate data based off the max size
+            for (int i = 0; i < _MAX_SIZE; i++)
+            {
+                // Generate Indexes for Dictionaries
+                int nameIndex = _Rand.Next() % _NameDict.Count;
+                if (nameIndex >= _NameDict.Count)
+                    throw new ArgumentOutOfRangeException("nameIndex");
+
+                int addressIndex = _Rand.Next() % _AddressDict.Count;
+                if (addressIndex >= _AddressDict.Count)
+                    throw new ArgumentOutOfRangeException("addressIndex");
+
+                // Generate the DTO row
+                FileDTO row = new FileDTO(
+                    _Rand.Next(0, 500),
+                    _NameDict[nameIndex],
+                    _AddressDict[addressIndex],
+                    _Rand.Next(1111111, 9999999));
+                _Table.Add(row);
+            }
+        }
+
+        public void WriteFile(string filename)
+        {
+            try
+            {
+                // Serialize Table
+                string serializedTable = JsonConvert.SerializeObject(_Table);
+                // Write File
+                File.WriteAllText(filename, serializedTable);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e.Message}");
+            }
+        }
+    }
+}
---
+++ CS260_Redux_II/CS260_Redux/Model/FileDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS260_Redux.Model
+{
+    public class FileDTO
+    {
+        public int key { get; set; }
+        public String name { get; set; }
+        public String address { get; set; }
+        public int phoneNumber { get; set; }
+
+        public FileDTO(int k, string n, string a, int p)
+        {
+            key = k;
+            name = n;
+            address = a;
+            phoneNumber = p;
+        }
+    }
+}
