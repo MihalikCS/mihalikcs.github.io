4245bf9577aca0b91705a91a4220e163791ba126
Michael Mihalik <45341659+mmihalikcs@users.noreply.github.com>
2021-04-25 21:56:43 -0400

    Add files via upload

---
+++ CS260_Redux/CS260_Redux/Generator/DataGenerator.cs
@@ -0,0 +1,56 @@
+﻿using CS260_Redux.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS260_Redux.Generator
+{
+    public class DataGenerator
+    {
+        int _MAX_SIZE;
+        List<FileDTO> _Table;
+        Random _Rand;
+
+        public DataGenerator(int size)
+        {
+            _MAX_SIZE = size;
+            _Table = new List<FileDTO>(1000);
+
+            // Seed Rand
+            _Rand = new Random();
+        }
+
+        public void GenerateData()
+        {
+            // Generate data based off the max size
+            for (int i = 0; i < _MAX_SIZE; i++)
+            {
+                FileDTO row = new FileDTO(
+                    _Rand.Next(0, 500),
+                    "David",
+                    "Dog",
+                    _Rand.Next(1111111, 9999999));
+                _Table.Add(row);
+            }
+        }
+
+        public void WriteFile(string filename)
+        {
+            try
+            {
+                // Serialize Table
+                string serializedTable = JsonConvert.SerializeObject(_Table);
+                // Write File
+                File.WriteAllText(filename, serializedTable);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e.Message}");
+            }
+        }
+    }
+}
---
+++ CS260_Redux/CS260_Redux/Generator/DataLoader.cs
@@ -0,0 +1,40 @@
+﻿using CS260_Redux.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS260_Redux.Generator
+{
+    public class DataLoader
+    {
+        List<FileDTO> _TestData;
+        string _DataFilename;
+
+        public DataLoader(string testFileName)
+        {
+            _DataFilename = testFileName;
+            _TestData = new List<FileDTO>();
+        }
+
+        public void LoadTestData()
+        {
+            // Get String
+            var serializedString = File.ReadAllText(_DataFilename);
+            // Deserialize & Load
+            _TestData = JsonConvert.DeserializeObject<List<FileDTO>>(serializedString);
+        }
+
+        public List<FileDTO> getTestData()
+        {
+            // Force Load
+            if (_TestData.Count <= 0)
+                LoadTestData();
+            // Return the item
+            return _TestData;
+        }
+    }
+}
